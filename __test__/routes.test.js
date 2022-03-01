c48e28c606bf2717f6300e80a6dc17a201bc01b8
LinaYahya <linaebe0@gmail.com>
2022-03-01 10:00:59 +0200

    test failed

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,13 +1,13 @@
-const request = require('supertest');
-const app = require('../server/app');
+const request = require("supertest");
+const app = require("../server/app");
 
-test('What your tests is testing', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) return done(err);
-        expect(res.body.msg).toBe('welcome to github actions')
-        return done();
-      })
-    })
+test("What your tests is testing", (done) => {
+  request(app)
+    .get("/")
+    .expect(400)
+    .end(function (err, res) {
+      if (err) return done(err);
+      expect(res.body.msg).toBe("welcome to github actions");
+      return done();
+    });
+});
