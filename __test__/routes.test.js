f606451034f18bf3e0fd0ac2704b9b3037850c7c
LinaYahya <linaebe0@gmail.com>
2022-03-01 11:21:46 +0200

    let test fail

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -9,7 +9,7 @@ beforeAll(() => {
 test("What your tests is testing", (done) => {
   request(app)
     .get("/")
-    .expect(200)
+    .expect(400)
     .end(function (err, res) {
       if (err) return done(err);
       expect(res.body.msg).toBe("welcome to github actions");
