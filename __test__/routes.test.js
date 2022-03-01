a88a31c400ac2e01b9795dfa85da13a25367029b
LinaYahya <linaebe0@gmail.com>
2022-03-01 10:09:43 +0200

    test get books route

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -4,10 +4,22 @@ const app = require("../server/app");
 test("What your tests is testing", (done) => {
   request(app)
     .get("/")
-    .expect(400)
+    .expect(200)
     .end(function (err, res) {
       if (err) return done(err);
       expect(res.body.msg).toBe("welcome to github actions");
+      return done();
+    });
+});
+
+test("test our database", (done) => {
+  request(app)
+    .get("/books")
+    .expect(200)
+    .end(function (err, res) {
+      if (err) return done(err);
+      console.log(res.body);
+      expect(res.body[0].name).toBe("A Thousand Splendid Suns");
       return done();
     });
 });
