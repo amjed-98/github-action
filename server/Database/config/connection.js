97d468472ef67ee0463e3b4d9ee465408725dcf8
LinaYahya <linaebe0@gmail.com>
2022-03-01 10:47:39 +0200

    add before and afterAll DB teting

--- server/Database/config/connection.js
+++ server/Database/config/connection.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
-let dbUrl = '';
-const { NODE_ENV, DB_URL_TEST, DB_URL_DEV, DATABASE_URL } = process.env
+let dbUrl = "";
+const { NODE_ENV, POSTGRES_DB, DB_URL_DEV, DATABASE_URL } = process.env;
 
-if (NODE_ENV === 'test') {
-  dbUrl = DB_URL_TEST;
-} else if (NODE_ENV === 'development') {
+if (NODE_ENV === "test") {
+  dbUrl = POSTGRES_DB;
+} else if (NODE_ENV === "development") {
   dbUrl = DB_URL_DEV;
 } else {
   dbUrl = DATABASE_URL;
 }
-console.log(dbUrl, 'connection')
+console.log(dbUrl, "connection");
 const option = {
   connectionString: dbUrl,
-  ssl: NODE_ENV === "test" ? false :{ rejectUnauthorized: false },
+  ssl: NODE_ENV === "test" ? false : { rejectUnauthorized: false },
 };
 
 module.exports = new Pool(option);
