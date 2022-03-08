081b0569f76d18e5fb82d9f2f5705d3f8b34f6ca
LinaYahya <linaebe0@gmail.com>
2022-03-08 10:25:56 +0200

    test get books route

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,5 +1,15 @@
 const request = require('supertest');
 const app = require('../server/app');
+const dbBuild = require('../server/Database/config/build');
+const connection = require('../server/Database/config/connection');
+
+beforeAll(()=>{
+  return dbBuild()
+})
+
+afterAll(()=> {
+  return connection.end();
+})
 
 test('What your tests is testing', (done) => {
     request(app)
@@ -11,3 +21,15 @@ test('What your tests is testing', (done) => {
         return done();
       })
     })
+
+test("test route get books", (done) => {
+    request(app)
+      .get('/books')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        console.log(res.body)
+        expect(res.body.length).toBe(3)
+        return done();
+      })
+})
